c6b2ac5aef069781a1ad7f186b13a010dfaca47b
Masud <masudrahman.dev@gmail.com>
2024-03-17 19:42:45 +0600

    first time make done

---
+++ src/components/icons/circle-icon/CircleIcon.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const CircleIcon = () => {
+  return (
+    <svg
+      width="19"
+      height="19"
+      viewBox="0 0 19 19"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="9.5" cy="9.5" r="9" fill="white" stroke="#121212" />
+      <circle cx="9.5" cy="9.5" r="5.5" fill="#121212" />
+      <circle cx="9.5" cy="9.5" r="9" fill="white" stroke="#00893C" />
+      <circle cx="9.5" cy="9.5" r="5.5" fill="#00893C" />
+      <circle cx="9.5" cy="9.5" r="9" fill="white" stroke="#121212" />
+      <circle cx="9.5" cy="9.5" r="5.5" fill="#121212" />
+      <circle cx="9.5" cy="9.5" r="9" fill="#008930" stroke="#00893C" />
+      <path
+        d="M8.59503 13.8266C8.48048 13.9379 8.3242 14 8.16185 14C7.99951 14 7.84323 13.9379 7.72868 13.8266L4.26926 10.4847C3.91025 10.1379 3.91025 9.57558 4.26926 9.22947L4.70243 8.81096C5.06156 8.4642 5.64304 8.4642 6.00206 8.81096L8.16185 10.8972L13.9979 5.26007C14.3571 4.91331 14.9391 4.91331 15.2976 5.26007L15.7307 5.67858C16.0898 6.02535 16.0898 6.58754 15.7307 6.93377L8.59503 13.8266Z"
+        fill="white"
+      />
+      <circle cx="9.5" cy="9.5" r="9" fill="white" stroke="#00893C" />
+      <circle cx="9.5" cy="9.5" r="5.5" fill="#00893C" />
+    </svg>
+  );
+};
+
+export default CircleIcon;
--- src/components/timeline/Timeline.tsx
+++ src/components/timeline/Timeline.tsx
@@ -2,86 +2,69 @@ import React from "react";
 import * as Separator from "@radix-ui/react-separator";
 import Title from "../atoms/title/Title";
 import PendingIcon from "../icons/panding-icon/PandingIcon";
+import CircleIcon from "../icons/circle-icon/CircleIcon";
 
+const Icon = (
+  <svg
+    width="32"
+    height="32"
+    viewBox="0 0 32 32"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M24.0261 14.6782C23.3816 14.6782 22.7566 14.766 22.1561 14.9223V8.10142C22.1561 8.05261 22.1316 7.9403 22.0877 7.86705C22.0877 7.86705 22.0877 7.86705 22.0877 7.86217C22.0291 7.75967 21.9363 7.68155 21.8191 7.64736L11.4926 2.19361C11.3509 2.12036 11.1801 2.12036 11.0384 2.19361C9.93987 2.77461 1.95206 6.99317 0.711875 7.64736C0.501937 7.71573 0.375 7.93055 0.375 8.10142V22.5732C0.375 22.7588 0.4775 22.9248 0.63375 23.0078L11.0384 28.4957C11.0824 28.5202 11.1947 28.5543 11.2679 28.5543C11.3363 28.5543 11.4437 28.525 11.4926 28.4957L17.1709 25.5027C18.3867 28.0759 20.9989 29.8629 24.026 29.8629C28.2152 29.8629 31.6232 26.4549 31.6232 22.2705C31.6233 18.0813 28.2153 14.6782 24.0261 14.6782ZM11.268 3.17986L20.6229 8.11611L16.2726 10.4158L7.44987 5.19142L11.268 3.17986ZM11.268 13.0572L1.90819 8.11605L6.43919 5.72848L15.2571 10.9479L11.268 13.0572ZM21.1796 15.2348C18.4014 16.3627 16.4386 19.0871 16.4386 22.2705C16.4386 23.081 16.5656 23.8573 16.8048 24.5897L11.7563 27.2555V13.9018L15.7551 11.7925V15.3373C15.7551 15.6107 15.9749 15.8255 16.2434 15.8255C16.5168 15.8255 16.7316 15.6107 16.7316 15.3373V11.275L21.1796 8.92655V15.2348H21.1796ZM26.7798 25.3221C26.6187 25.5369 26.3111 25.5809 26.0962 25.4198L23.1862 23.2275C23.0642 23.1348 22.9909 22.9932 22.9909 22.8369V17.7151C22.9909 17.4417 23.2107 17.2269 23.4792 17.2269C23.7477 17.2269 23.9674 17.4417 23.9674 17.7151V22.5928L26.6821 24.6386C26.897 24.7997 26.9409 25.1073 26.7798 25.3221Z"
+      fill={"#00622B"}
+    />
+  </svg>
+);
 const Timeline = () => {
   return (
     <div className="rounded-xl bg-white p-6">
+      <Title>Product Timeline</Title>
       <div>
-        <section className="">
-          <Title>Product Timeline</Title>
-          <div className="mt-6">
-            <ul>
-              <li className="relative flex items-baseline gap-6 pb-5">
-                <div className="before:absolute before:left-[5.5px] before:h-full before:w-[1px] before:bg-gray-400">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="12"
-                    height="12"
-                    className="bi bi-circle-fill fill-gray-400"
-                    viewBox="0 0 16 16"
-                  >
-                    <circle cx="8" cy="8" r="8" />
-                  </svg>
-                </div>
-                <div>
-                  <p className="text-sm text-gray-600">18-8-2022</p>
-                  <p className="mt-2 text-sm text-gray-600">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Maiores incidunt blanditiis dignissimos, enim earum
-                    mollitia. dolor sit amet, consectetur adipisicing elit.
-                    Maiores incidunt blanditiis dignissimos, enim earum
-                    mollitia.dolor sit amet, consectetur adipisicing elit.
-                    Maiores incidunt blanditiis dignissimos, enim earum
-                    mollitia.dolor sit amet, consectetur adipisicing elit.
-                    Maiores incidunt blanditiis dignissimos, enim earum
-                    mollitia.dolor sit amet, consectetur adipisicing elit.
-                    Maiores incidunt blanditiis dignissimos, enim earum
-                    mollitia.
-                  </p>
-                </div>
-              </li>
-              <li className="relative flex items-baseline gap-6 pb-5">
-                <div className="before:absolute before:left-[5.5px] before:h-full before:w-[1px] before:bg-gray-400">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="12"
-                    height="12"
-                    className="bi bi-circle-fill fill-gray-400"
-                    viewBox="0 0 16 16"
-                  >
-                    <circle cx="8" cy="8" r="8" />
-                  </svg>
-                </div>
-                <div>
-                  <p className="text-sm text-gray-600">18-8-2022</p>
-                  <p className="mt-2 text-sm text-gray-600">
-                    Lorem ipsum dolor sit amet.
-                  </p>
-                </div>
-              </li>
-              <li className="relative flex items-baseline gap-6 pb-5">
-                <div>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="12"
-                    height="12"
-                    className="bi bi-circle-fill fill-gray-400"
-                    viewBox="0 0 16 16"
-                  >
-                    <circle cx="8" cy="8" r="8" />
-                  </svg>
-                </div>
-                <div>
-                  <p className="text-sm text-gray-600">18-8-2022</p>
-                  <p className="mt-2 text-sm text-gray-600">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Est?
-                  </p>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </section>
+        <ol className="relative border-s ">
+          <li className="ms-6">
+            <span className="absolute -start-2 flex h-6 w-6 items-center justify-center rounded-full bg-white ">
+              {Icon}
+            </span>
+            <h3 className="">
+              <span className="   text-base font-bold text-msGreen">
+                Delivered
+              </span>
+            </h3>
+          </li>
+          <li className="ms-6 mt-4 ">
+            <span className="absolute -start-3 flex h-6 w-6 items-center justify-center rounded-full bg-white text-center ">
+              <CircleIcon />
+            </span>
+
+            <div className="">
+              <time className="block text-silver ">
+                19 January 2024 at 10:35am
+              </time>
+
+              <p className="pb-3 pt-2">Your product has been delivered.</p>
+              <div className="flex items-center gap-2">
+                <img
+                  src="https://picsum.photos/50/50"
+                  className=" rounded-md"
+                  alt=""
+                />
+                <img
+                  src="https://picsum.photos/50/50"
+                  className=" rounded-md"
+                  alt=""
+                />
+                <img
+                  src="https://picsum.photos/50/50"
+                  className=" rounded-md"
+                  alt=""
+                />
+              </div>
+            </div>
+          </li>
+        </ol>
       </div>
     </div>
   );
